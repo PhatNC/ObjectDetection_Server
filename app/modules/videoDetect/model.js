3c6c7c6ebbbfa4642877d298d16030ec4bb530f0
PHÁT NGUYỄN CƯỜNG <15520601@ms.uit.edu.vn>
2019-09-23 21:28:56 +0700

    server

--- app/modules/videoDetect/model.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('../../helpers/connection');
-// const Schema = mongoose.Schema;
-
-// const videoSchema = Schema({
-//     name: { type: String, required: true },
-//     path: { type: String, required: true }    
-// });
-// module.exports = mongoose.model('Video', videoSchema);
